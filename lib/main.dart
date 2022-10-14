7643e13b1f8e30aded68c274a187c5e843f35829
Abdumutalibovo <odilbekjuraev07@gmail.com>
2022-10-14 18:58:21 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'BentonSansBold',
       ),
       debugShowCheckedModeBanner: false,
-      home: callPage(),
+      home: UploadPage(),
     );
   }
 }
